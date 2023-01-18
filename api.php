0e6e0cc41d00946aeda1d428c426846c93b759ba
LaPoloche <buchon.paul@gmail.com>
2023-01-18 23:01:08 +0100

    a

---
+++ api.php
@@ -0,0 +1,33 @@
+<?php
+// Connexion à la base de données
+$host = "127.0.0.1:3306";
+$user = "admin";
+$password = "Base2donnée";
+$dbname = "cook_project";
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Requête pour récupérer les données de la table "product"
+$sql = "SELECT * FROM product";
+$result = $conn->query($sql);
+
+// Vérifier s'il y a des résultats
+if ($result->num_rows > 0) {
+    // Boucle pour créer un tableau des résultats
+    $data = array();
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    // Encode le tableau en format JSON
+    echo json_encode($data);
+} else {
+    echo "0 results";
+}
+
+// Fermeture de la connexion
+$conn->close();
+?>
