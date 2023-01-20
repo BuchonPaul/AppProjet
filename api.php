309e4e630838a388d9f8f5e1af4cd0bde03a99ff
LaPoloche <buchon.paul@gmail.com>
2023-01-20 10:31:54 +0100

    a

--- api.php
+++ api.php
@@ -1,33 +1,26 @@
 <?php
-// Connexion à la base de données
 $host = "127.0.0.1:3306";
 $user = "u693686849_admin";
 $password = "Base2donnée";
 $dbname = "u693686849_cook_project";
-$conn = new mysqli($host, $user, $password, $dbname);
+// global $CONN = new mysqli($host, $user, $password, $dbname);
 
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// function get_all_product(){
+//     $sql = "SELECT * FROM product";
+//     $result = $CONN->query($sql);
+//     if ($result->num_rows > 0) {
+//         $data = array();
+//         while($row = $result->fetch_assoc()) {
+//             $data[] = $row;
+//         }
+//         echo json_encode($data);
+//     } else {
+//         echo "0 results";
+//     }
+//     $CONN->close();
+// }
 
-// Requête pour récupérer les données de la table "product"
-$sql = "SELECT * FROM product";
-$result = $conn->query($sql);
-
-// Vérifier s'il y a des résultats
-if ($result->num_rows > 0) {
-    // Boucle pour créer un tableau des résultats
-    $data = array();
-    while($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-    // Encode le tableau en format JSON
-    echo json_encode($data);
-} else {
-    echo "0 results";
-}
-
-// Fermeture de la connexion
-$conn->close();
 ?>
