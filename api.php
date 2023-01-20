3c4bab890c6b21638c7c2afd1dfe234a2af1497a
LaPoloche <buchon.paul@gmail.com>
2023-01-20 10:37:12 +0100

    s

--- api.php
+++ api.php
@@ -3,24 +3,25 @@ $host = "127.0.0.1:3306";
 $user = "u693686849_admin";
 $password = "Base2donnée";
 $dbname = "u693686849_cook_project";
-// global $CONN = new mysqli($host, $user, $password, $dbname);
+$CONN = new mysqli($host, $user, $password, $dbname);
 
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-// function get_all_product(){
-//     $sql = "SELECT * FROM product";
-//     $result = $CONN->query($sql);
-//     if ($result->num_rows > 0) {
-//         $data = array();
-//         while($row = $result->fetch_assoc()) {
-//             $data[] = $row;
-//         }
-//         echo json_encode($data);
-//     } else {
-//         echo "0 results";
-//     }
-//     $CONN->close();
-// }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+function get_all_product(){
+    $sql = "SELECT * FROM product";
+    $result = $GLOBALS['CONN']->query($sql);
+    if ($result->num_rows > 0) {
+        $data = array();
+        while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return json_encode($data);
+    } else {
+        return "0 results";
+    }
+}
+var_dump(get_all_product());
+$CONN->close();
 
 ?>
