74e41ac5b77133174aa6d319bbcb30e16324246e
LaPoloche <buchon.paul@gmail.com>
2023-01-28 18:59:53 +0100

    sd

--- index.php
+++ index.php
@@ -27,7 +27,9 @@ require_once("api.php");
             </form>
         <?php 
         $table = json_decode(get_all_product());
-        var_dump($table[1][0]);
+        var_dump($table[1]);
+        var_dump("test");
+        var_dump($table[1]["id"]);
             foreach($table as $key=>$value) {
                 var_dump($table[$key]["id"][1][0]);
             }
